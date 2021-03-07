bdd5bcd67eb1a31be8cf747f495198400ae8b4ea
cmllr <fury224@googlemail.com>
2021-03-07 13:17:49 +0100

    Spaawn cronjob on start

--- webgui/apps.py
+++ webgui/apps.py
@@ -1,6 +1,23 @@
 from django.apps import AppConfig
+from django.core import management
+from django.core.management.commands import loaddata
+from threading import Thread
 
 
 class WebguiConfig(AppConfig):
     name = "webgui"
     verbose_name = "APX Administration"
+    kill_thread = False
+
+    def background_cron(self):
+        while not self.kill_thread:
+            management.call_command("cron")
+        print("Bye")
+
+    def ready(self):
+        # call cron job module
+        try:
+            worker = Thread(target=self.background_cron, daemon=True)
+            worker.start()
+        except KeyboardInterrupt:
+            kill_thread = True
