267ff41d01502182a02ab0a4021a72de091dbfe1
cmllr <fury224@googlemail.com>
2021-04-02 12:07:09 +0200

    Added mock data

--- wizard.bat
+++ wizard.bat
@@ -7,10 +7,12 @@ if not exist "db.sqlite3" (
 	mkdir "uploads/logs"
 	mkdir "uploads/keys"
 	cp "wizard/settings.py.tpl" "wizard/settings.py"
+	cp "default.rfm" "uploads/default.rfm"
   python.exe get-pip.py
   python.exe -m pip install -r requirements.txt
 	python.exe manage.py migrate
 	python.exe manage.py createsuperuser
+	python.exe manage.py loaddata fixture.json
 ) else (
 	python.exe manage.py runserver
 )
