00c0c6ee7bb0b99e86210d3ea41f5a72c0ea1a6d
cmllr <fury224@googlemail.com>
2021-05-09 08:44:43 +0200

    Fix issues in wizard.bat

--- wizard.bat
+++ wizard.bat
@@ -4,10 +4,10 @@ python.exe --version
 if not exist "db.sqlite3" (
 	mkdir packs
 	mkdir uploads
-	mkdir "uploads/logs"
-	mkdir "uploads/keys"
-	copy "wizard/settings.py.tpl" "wizard/settings.py"
-	copy "default.rfm" "uploads/default.rfm"
+	mkdir "uploads\logs"
+	mkdir "uploads\keys"
+	copy "wizard\settings.py.tpl" "wizard\settings.py"
+	copy "default.rfm" "uploads\default.rfm"
   python.exe get-pip.py
   python.exe -m pip install -r requirements.txt
 	python.exe manage.py migrate
