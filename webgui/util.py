78881438bf21264e6e81abae5596f3ad7384e4bc
cmllr <fury224@googlemail.com>
2021-07-30 11:39:24 +0200

    Add correct release link

--- webgui/util.py
+++ webgui/util.py
@@ -53,7 +53,7 @@ FILE_NAME_SUFFIXES = [
 
 
 RECIEVER_COMP_INFO = open(join(BASE_DIR, "release")).read()
-RECIEVER_DOWNLOAD_FROM = "http://localhost:8282/reciever-2021R56.zip"  # DEBUG ONLY
+RECIEVER_DOWNLOAD_FROM = "https://github.com/apx-simracing/reciever/releases/download/R57/reciever-2021R57.zip"  # DEBUG ONLY
 
 
 def get_update_filename(instance, filename):
