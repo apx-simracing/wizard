8391bdd212d22b005c8cc67d082e0d4cb4a65929
cmllr <fury224@googlemail.com>
2021-09-05 12:10:06 +0200

    Refine import command

--- webgui/management/commands/import.py
+++ webgui/management/commands/import.py
@@ -1,38 +1,90 @@
 from django.core.management.base import BaseCommand
 from os.path import exists, join
 from os import mkdir, listdir
-from wizard.settings import BASE_DIR
+from wizard.settings import BASE_DIR, MEDIA_ROOT
 from webgui.models import Component, Entry, EntryFile, TrackFile, Track
-from webgui.util import FILE_NAME_SUFFIXES
+from webgui.util import FILE_NAME_SUFFIXES, FILE_NAME_SUFFIXES_MEANINGS
 from shutil import copyfile
+from re import search
 
 
 class Command(BaseCommand):
     help = "Import data"
 
+    def get_file_meaning(self, filename):
+        selected_suffix = None
+        for index, suffix in enumerate(FILE_NAME_SUFFIXES):
+            if str(filename).endswith(suffix):
+                selected_suffix = FILE_NAME_SUFFIXES_MEANINGS[index]
+        return selected_suffix
+
+    def get_vehicle_filename(
+        self, filename, component_name, short_name, vehicle_number, full_path=True
+    ):
+
+        full_user_path = join(MEDIA_ROOT)
+        if not exists(full_user_path):
+            mkdir(full_user_path)
+
+        liveries_path = join(full_user_path, "liveries")
+        if not exists(liveries_path):
+            mkdir(liveries_path)
+
+        component_path = join(liveries_path, component_name)
+        if not exists(component_path):
+            mkdir(component_path)
+
+        selected_suffix = None
+        for suffix in FILE_NAME_SUFFIXES:
+            if str(filename).endswith(suffix):
+                selected_suffix = suffix
+        if selected_suffix is None:
+            raise ValidationError("We can't identify that file purpose")
+        if not full_path:
+            return "{}_{}{}".format(short_name, vehicle_number, selected_suffix)
+        new_file_path = join(
+            "liveries",
+            component_name,
+            "{}_{}{}".format(short_name, vehicle_number, selected_suffix),
+        )
+        return new_file_path
+
     def handle(self, *args, **options):
-        kind_input = input('Name the kind of the content "track" or "vehicle": ')
-        if kind_input not in ["track", "vehicle"]:
-            raise Exception("Invalid input")
         import_path = join(BASE_DIR, "import")
         if not exists(import_path):
             mkdir(import_path)
 
-        print(f"Importing: {kind_input}")
         short_name = input("Name a valid short name to identify the files: ")
-        is_vehicle = kind_input == "vehicle"
-        search_type = "VEH" if is_vehicle else "LOC"
-        entries = Component.objects.filter(short_name=short_name, type=search_type)
+        entries = Component.objects.filter(short_name=short_name)
         if entries.count() != 1:
             raise Exception(
-                f"Did not manage to find a {kind_input} with short name {short_name}."
+                f"Did not manage to find a component with short name {short_name}."
+            )
+        else:
+            entry = entries.first()
+            print(
+                "We will use the component {} for this import".format(
+                    entry.component_name
+                )
             )
 
+        is_vehicle = entries.first().type == "VEH"
         files = listdir(import_path)
         if not is_vehicle:
             layout = input("Name the layout: ")
+            track = Track.objects.filter(
+                component=entries.first(), layout=layout
+            ).first()
+            if track is None:
+                raise Exception(f"No track found with layout {layout}")
+            print("Component {}, layout {}".format(entries.first(), layout))
             for file in files:
-                track = Track.objects.get(component=entries.first(), layout=layout)
+                print(f"\t File {file}")
+
+            confirm = input("Is this okay? Y/N: ")
+            if confirm.lower() != "y":
+                raise Exception("Abort.")
+            for file in files:
                 track_file = TrackFile()
                 track_file.track = track
 
@@ -55,33 +107,70 @@ class Command(BaseCommand):
                 copyfile(source_path, target_path)
 
         if is_vehicle:
-            suffix = input(
-                f"Do you have any chars between the {short_name} and the car number? If not, just press enter: "
-            )
+            regex_map = {
+                "anum": r"(?P<number>[a-zA-Z]?\d+[a-zA-Z]?)",
+                "num": r"(?P<number>\d+)",
+            }
+            got = input("Can entries be alphanumeric (e. g. '13x')? Y/N: ")
 
-            prefix = short_name + suffix
-            # search for vehicles matching the short name
-            matched_files = list(filter(lambda x: prefix in x, files))
+            pattern = regex_map["num"]
+            if got.lower() == "y":
+                pattern = regex_map["anum"]
+
+            team_name = entries.first()
 
             # group files
             file_groups = {}
 
-            for file in matched_files:
-                file_without_prefix = file.replace(prefix, "")
-                # attempt to find the name of the file without suffix and extensions
+            for file in files:
+                matches = search(pattern, file)
+                number = None
+                if matches:
+                    number = matches.group("number")
+                if number is not None:
+                    if number not in file_groups:
+                        file_groups[number] = []
+                    file_groups[number].append(file)
+                else:
+                    raise Exception(f"Could not identify file: {file}")
 
-                for suffix in FILE_NAME_SUFFIXES:
-                    lower_suffix = suffix.lower()
-                    file_lower = file_without_prefix.lower()
-                    only_number = file_lower.replace(lower_suffix, "")
-                    if (
-                        only_number != file_lower and "region" not in only_number
-                    ):  # the file actually matched
-                        if only_number not in file_groups:
-                            file_groups[only_number] = []
-                        if file not in file_groups[only_number]:
-                            file_groups[only_number].append(file)
+            team_names_add = input("Do you want to set team names? Y/N: ")
+            if team_names_add.lower() == "y":
+                new_groups = {}
+                for number, files in file_groups.items():
+                    new_name = input(
+                        f"Name of team with car #{number} (enter to omit): "
+                    )
+                    if new_name:
+                        new_groups[f"{new_name}#{number}"] = files
+                    else:
+                        new_groups[number] = files
+                file_groups = new_groups
 
+            for number, files in file_groups.items():
+                print(f"Entry {number}:")
+                for file in files:
+                    new_name = self.get_vehicle_filename(
+                        file,
+                        entries.first().component_name,
+                        entries.first().short_name,
+                        number,
+                        False,
+                    )
+                    if new_name != file:
+                        print(
+                            f"\t File {file} => {new_name}: "
+                            + "({})".format(self.get_file_meaning(file))
+                        )
+                    else:
+                        print(
+                            f"\t File {file}: "
+                            + "({})".format(self.get_file_meaning(file))
+                        )
+            confirm = input("Is this okay? Y/N: ")
+
+            if confirm.lower() != "y":
+                raise Exception("Abort.")
             for number, files in file_groups.items():
                 print(f"Processing matches for car {number}")
                 existing_entries = Entry.objects.filter(
@@ -91,17 +180,24 @@ class Command(BaseCommand):
                     raise Exception("The entry is already existing for this component")
                 e = Entry()
                 e.component = entries.first()
-                e.team_name = e.component.component_name
-                e.vehicle_number = number
+                if "#" not in number:
+                    e.team_name = team_name
+                    e.vehicle_number = number
+                else:
+                    parts = number.split("#")
+                    e.team_name = parts[0]
+                    e.vehicle_number = parts[1]
                 e.save()
                 for file in files:
                     print(f"Adding file {file} to entry of car {number}")
                     e_f = EntryFile()
                     e_f.entry = e
-                    relative_path = join("liveries", e.component.component_name, file)
-                    e_f.file = join(relative_path)
                     source_path = join(BASE_DIR, "import", file)
-                    target_path = join(BASE_DIR, "uploads", relative_path)
+                    file_name = self.get_vehicle_filename(
+                        file, e.component.component_name, e.component.short_name, number
+                    )
+                    e_f.file = file_name
+                    target_path = join(BASE_DIR, "uploads", file_name)
                     print(f"Copied {file} to {target_path}")
                     e_f.save()
                     copyfile(source_path, target_path)
