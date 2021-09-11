d5b8365697097a8c885142f1a714e1219b86a954
cmllr <fury224@googlemail.com>
2021-09-11 13:22:37 +0200

    Update reciever URL

--- webgui/util.py
+++ webgui/util.py
@@ -87,7 +87,7 @@ FILE_NAME_SUFFIXES_MEANINGS = [
 ]
 
 RECIEVER_COMP_INFO = open(join(BASE_DIR, "release")).read()
-RECIEVER_DOWNLOAD_FROM = "https://github.com/apx-simracing/reciever/releases/download/R71/reciever-2021R71.zip"
+RECIEVER_DOWNLOAD_FROM = "https://github.com/apx-simracing/reciever/releases/download/R72/reciever-2021R72.zip"
 
 
 def get_update_filename(instance, filename):
